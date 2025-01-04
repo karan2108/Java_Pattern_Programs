9cacae6d60888cd6663ec62436545842e4d190b8
karan2108 <77577426+karan2108@users.noreply.github.com>
2025-01-04 22:50:14 +0530

    Star program

---
+++ Star.java
@@ -0,0 +1,36 @@
+
+/*Star Pattern 1
+
+* 
+
+* * 
+
+* * * 
+
+* * * * 
+
+* * * * *   */
+
+public class Star {
+
+  public static void main(String[] args) {
+
+  int rows = 5;
+
+  for (int i = 1; i <= rows; ++i)
+  {  //Outer loop for rows
+
+    for (int j = 1; j <= i; ++j) { //Inner loop for Col
+
+    System.out.print("* "); //Print *
+
+  }
+
+  System.out.println(); //New line
+
+  }
+
+ }
+
+}
+
