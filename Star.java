6418be97fada936d627246a4be7fdd0feebba6a1
karan2108 <77577426+karan2108@users.noreply.github.com>
2025-01-05 08:13:31 +0530

    Update Star.java

--- Star.java
+++ Star.java
@@ -1,36 +1,25 @@
-
-/*Star Pattern 1
-
+/*
+Star Pattern :- 
 * 
-
 * * 
-
 * * * 
-
 * * * * 
-
-* * * * *   */
+* * * * *   
+*/
 
 public class Star {
+    public static void main(String[] args) {
+    int rows = 5;
+    for (int i = 1; i <= rows; ++i)
+    {  
+      //Outer loop for rows
+      for (int j = 1; j <= i; ++j) 
+      { 
+        //Inner loop for Col
+        System.out.print("* "); //Print *
+      }
 
-  public static void main(String[] args) {
-
-  int rows = 5;
-
-  for (int i = 1; i <= rows; ++i)
-  {  //Outer loop for rows
-
-    for (int j = 1; j <= i; ++j) { //Inner loop for Col
-
-    System.out.print("* "); //Print *
-
+      System.out.println(); //New line
+    }
   }
-
-  System.out.println(); //New line
-
-  }
-
- }
-
 }
-
